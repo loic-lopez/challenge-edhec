22436d255a6e4600686e994562167b8074337263
loiclopez <loic.lopez@epitech.eu>
2017-12-02 15:00:02 +0100

    onlick add

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,5 @@
 <header class="topbar">
+    <script src="/assets/plugins/jquery/jquery.min.js"></script>
     <nav class="navbar top-navbar navbar-expand-md navbar-light">
         <!-- ============================================================== -->
         <!-- Logo -->
@@ -6,7 +7,7 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="/">
                 <b>
-                    <img src="/assets/images/users/user.svg" height="80"/>
+                    <img src="/assets/images/users/user.svg" id="main-logo" style="height: 80px"/>
                 </b>
             </a>
         </div>
@@ -17,13 +18,23 @@
             <!-- ============================================================== -->
             <!-- toggle and nav items -->
             <!-- ============================================================== -->
+            <script>
+                function changeLogoSize() {
+                    console.log($("#main-logo").css("height"));
+                    if ($("#main-logo").css("height") === "80px")
+                        $('#main-logo').css('height', '30px');
+                    else
+                        $('#main-logo').css('height', '80px');
+
+                }
+            </script>
             <ul class="navbar-nav mr-auto mt-md-0">
                 <!-- This is  -->
                 <li class="nav-item"><a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark"
-                                        href="javascript:void(0)"><i class="mdi mdi-menu"></i></a></li>
+                                        href="javascript:void(0)" onclick="changeLogoSize();"><i class="mdi mdi-menu"></i></a></li>
                 <li class="nav-item m-l-10"><a
                             class="nav-link sidebartoggler hidden-sm-down text-muted waves-effect waves-dark"
-                            href="javascript:void(0)"><i class="ti-menu"></i></a></li>
+                            href="javascript:void(0)" onclick="changeLogoSize();"><i class="ti-menu"></i></a></li>
 
             </ul>
             <!-- ============================================================== -->
